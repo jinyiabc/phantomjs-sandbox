74f8bce81ab5557f4f8b0d54116976d808a686ea
Alex J.Y <33644601+jinyiabc@users.noreply.github.com>
2018-02-28 16:48:40 +0800

    update

--- hello.js
+++ hello.js
@@ -3,15 +3,15 @@
 // phantom.exit();
 
 //2. Network Requests and Responses
-// var page = require('webpage').create();
-// page.onResourceRequested = function(request) {
-//   console.log('Request ' + JSON.stringify(request, undefined, 4));
-// };
-// page.onResourceReceived = function(response) {
-//   console.log('Receive ' + JSON.stringify(response, undefined, 4));
-// };
-// page.open('www.google.com');
-// phantom.exit();
+var page = require('webpage').create();
+page.onResourceRequested = function(request) {
+  console.log('Request ' + JSON.stringify(request, undefined, 4));
+};
+page.onResourceReceived = function(response) {
+  console.log('Receive ' + JSON.stringify(response, undefined, 4));
+};
+page.open('www.google.com');
+phantom.exit();
 
 /*Request {
     "headers": [
